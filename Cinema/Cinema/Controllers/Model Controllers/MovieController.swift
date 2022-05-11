341242970d012d3b4fbad30668ca9e19d48f6bcc
Scott Cox <jscox5@gmail.com>
2022-05-11 10:19:38 -0600

    delete and update functions added

--- Cinema/Cinema/Controllers/Model Controllers/MovieController.swift
+++ Cinema/Cinema/Controllers/Model Controllers/MovieController.swift
@@ -8,14 +8,42 @@
 import Foundation
 import UIKit.UIImage
 
+// Our Model Controller Class
 class MovieController {
     
+    // Singleton: To ensure that we are interacting with the *SAME* instance of our *movies* array, we created a singleton and will pass around this specific instance of a *MovieController*
     static let sharedInstance = MovieController()
     
+    //MARK: - Source of Truth
+    // Source of truth: The collection where we want to store the data we protect. Collected via the array. Protected vie the singleton.
     var movies: [Movie] = []
     
+    
+    //MARK: - CRUD
+    // Creates a Movie object using the values from the user.
     func createMovie(name: String, rating: Double?, synopsis: String, poster: UIImage) {
+        // We initialize a movie object via the values passed in parameters.
         let movie = Movie(name: name, rating: rating, synopsis: synopsis, poster: poster)
+        // Now the the Movie is initialized we can add it to our SOT.
         movies.append(movie)
     }
-}
+    
+    
+    // What properties
+    // What movie
+    func update(newName: String, rating: Double?, synopsis: String, poster: UIImage, movieToUpdate: Movie) {
+        // The newName is assigned to the name value for the movie we want to update.
+        movieToUpdate.name = newName
+        movieToUpdate.rating = rating
+        movieToUpdate.synopsis = synopsis
+        movieToUpdate.moviePoster = poster
+     
+    }
+    
+    // What movie
+    func delete(movieToDelete: Movie) {
+        guard let indexOfMovieToDelete = movies.firstIndex(of: movieToDelete) else {return}
+        movies.remove(at: indexOfMovieToDelete)
+       
+    }
+} // End of class
--- Cinema/Cinema/Model/Movie.swift
+++ Cinema/Cinema/Model/Movie.swift
@@ -8,17 +8,39 @@
 import Foundation
 import UIKit
 
+/**
+ This is the blueprint for the Movie class. This initializes a Movie object.
+ It has properties:
+    - name: The *String * identifier for the movie
+    - rating: The *Double* used to display the rating of the Movie *Optional*
+    - synopsis: The *String* description of the Movie
+    - moviePoster: the *UIImage* for the Movies's promotional content
+ */
 class Movie {
     
-    let name: String
-    let rating: Double?
-    let synopsis: String
-    let moviePoster: UIImage
+    var name: String
+    var rating: Double?
+    var synopsis: String
+    var moviePoster: UIImage
     
+    
+    /**
+        The member-wise initializer Initializes, or creates, a Movie object using the data provided.
+     **/
     init(name:String, rating:Double?, synopsis: String, poster: UIImage){
         self.name = name
-        self.rating = rating
+        self.rating = rating // maybe caus its optional
         self.synopsis = synopsis
         self.moviePoster = poster
     }
 }
+
+extension Movie: Equatable {
+    static func == (lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.name == rhs.name &&
+        lhs.synopsis == rhs.synopsis &&
+        lhs.rating == rhs.rating &&
+        lhs.moviePoster == rhs.moviePoster
+    }
+}
+
